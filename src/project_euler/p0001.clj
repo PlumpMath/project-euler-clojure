ca8990deaa7463ab7fbb04d933049fbe05725a1e
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-07-20 09:43:47 +0800

    problem 0001: more optimize method
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- src/project_euler/p0001.clj
+++ src/project_euler/p0001.clj
@@ -9,6 +9,7 @@
 
 ;; Find the sum of all the multiples of 3 or 5 below 1000.
 
+;;;; Simplest solution
 (defn is-multiples-of-3-or-5?
   [x]
   (if (or (zero? (mod x 3))
@@ -16,4 +17,12 @@
     x 0))
 
 (reduce + (map is-multiples-of-3-or-5? (range 1 1000)))
+;; => 233168
+
+
+;;; More optimize method
+(-
+ (+ (reduce + (range 3 1000 3))
+    (reduce + (range 5 1000 5)))
+ (reduce + (range 15 1000 15)))
 ;; => 233168
