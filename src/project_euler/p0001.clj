37489a69dce4347e1e5806ab52cacb7483b90391
Yen-Chin Lee <coldnew.tw@gmail.com>
2016-11-23 13:31:23 +0800

    p0001: add core.logic solution
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- src/project_euler/p0001.clj
+++ src/project_euler/p0001.clj
@@ -1,4 +1,5 @@
-(ns project-euler.p0001)
+(ns project-euler.p0001
+  (:require [clojure.core.logic :as logic]))
 
 ;; https://projecteuler.net/problem=1
 ;;
@@ -12,11 +13,11 @@
 ;;;; Simplest solution
 (defn is-multiples-of-3-or-5?
   [x]
-  (if (or (zero? (mod x 3))
-          (zero? (mod x 5)))
-    x 0))
+  (or (zero? (mod x 3))
+      (zero? (mod x 5))))
 
-(reduce + (map is-multiples-of-3-or-5? (range 1 1000)))
+(reduce + (map #(if (is-multiples-of-3-or-5? %) % 0)
+               (range 1 1000)))
 ;; => 233168
 
 
@@ -25,4 +26,14 @@
  (+ (reduce + (range 3 1000 3))
     (reduce + (range 5 1000 5)))
  (reduce + (range 15 1000 15)))
+;; => 233168
+
+;;;; core.logic solution
+(logic/defnc logic-is-multiples-of-3-or-5? [x]
+  (is-multiples-of-3-or-5? x))
+
+(->> (logic/run* [x]
+       (logic/membero x (range 1 1000))
+       (logic-is-multiples-of-3-or-5? x))
+     (reduce +))
 ;; => 233168
